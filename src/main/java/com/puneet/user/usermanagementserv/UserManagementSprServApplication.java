8f39e367e3eab2f853ab456e7b19786db017c3e8
Puneet Pant <Puneet.Pant@walmartlabs.com>
2025-06-01 03:15:57 +0530

    Initial commit - with proj dependencies - spring-security,lombok,mysql,JPA to springboot project

---
+++ src/main/java/com/puneet/user/usermanagementserv/UserManagementSprServApplication.java
@@ -0,0 +1,13 @@
+package com.puneet.user.usermanagementserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSprServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementSprServApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puneet/user/usermanagementserv/UserManagementSprServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puneet.user.usermanagementserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSprServApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
