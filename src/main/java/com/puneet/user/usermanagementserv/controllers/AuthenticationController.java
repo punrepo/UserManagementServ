f3b22da78aaed4193444be45edf57e5eb3d0a460
Puneet Pant <pmpant16@gmail.com>
2025-08-31 23:31:27 -0700

    added Authentication controller

---
+++ src/main/java/com/puneet/user/usermanagementserv/controllers/AuthenticationController.java
@@ -0,0 +1,9 @@
+package com.puneet.user.usermanagementserv.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    //singup
+    //login
+}
